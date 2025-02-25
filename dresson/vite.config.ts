b5fb7446f9b2f795de25d3afd7760d8e4f479cbc
elay.glazer <elay.glazer@controlup.com>
2025-02-25 12:27:56 +0200

    revert base dresson to vite config ts

--- dresson/vite.config.ts
+++ dresson/vite.config.ts
@@ -1,15 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { chunkSplitPlugin } from 'vite-plugin-chunk-split' //  Use named import
 
 export default defineConfig({
-  plugins: [
-    react(),
-    chunkSplitPlugin({
-      strategy: 'all-in-one', //  Automatically splits large chunks
-    }),
-  ],
+  base: "/dresson-dev/", // Ensure this matches your repo name on GitHub Pages
+  plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 2100, //  Suppresses warning for large files
+    chunkSizeWarningLimit: 2600, // Optional: Suppress chunk size warnings
   },
 })
