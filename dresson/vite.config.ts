fe924b2e67625c0100a104f2340f879a39e028e1
elay.glazer <elay.glazer@controlup.com>
2025-02-25 11:07:43 +0200

    revert base dresson to vite config ts

--- dresson/vite.config.ts
+++ dresson/vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  base: "/dresson-dev/", //  Set this to match your GitHub Pages repo
   plugins: [react()],
 })
